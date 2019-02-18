0591f59d25b37dcf8d7bb8c3425f29d37a486e5e
zigitn <gwws@5a6c.me>
2019-02-18 16:46:16 +0800

    14

--- easy/014-length-of-last-word.go
+++ easy/014-length-of-last-word.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s := " "
+	fmt.Println(lengthOfLastWord(s))
+}
+
+func lengthOfLastWord(s string) int {
+	a := strings.Fields(s)
+	if len(a) == 0 {
+		return 0
+	}
+	return len(a[len(a)-1])
+}
