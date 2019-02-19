8b80aaa0ec3c67e0289d3d3c4f477213d86e8eb6
zigitn <gwws@5a6c.me>
2019-02-19 18:56:52 +0800

    21

---
+++ easy/021-same-tree.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	isSameTree(nil, nil)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p != nil && q != nil && p.Val == q.Val {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	} else {
+		return false
+	}
+}
