b92dc46ce67beabcb3ed8069831b5a8768e4a518
z <gwws@5a6c.me>
2019-03-06 20:58:46 +0800

    24 leetcode评论区代码的改进版本，可以按正确的顺序获取节点的值

--- easy/024-binary-tree-level-order-traversal-ii.go
+++ easy/024-binary-tree-level-order-traversal-ii.go
@@ -12,13 +12,13 @@ type TreeNode struct {
 
 func main() {
 	// node1 := TreeNode{1, nil, nil}
-	node2 := TreeNode{7, nil, nil}
-	node3 := TreeNode{15, nil, nil}
-	node4 := TreeNode{20, &node2, &node3}
+	node2 := TreeNode{5, nil, nil}
+	node3 := TreeNode{4, nil, nil}
+	node4 := TreeNode{3, nil, nil}
 
 	// node6 := TreeNode{6, nil, nil}
-	node7 := TreeNode{9, nil, nil}
-	node8 := TreeNode{3, &node7, &node4}
+	node7 := TreeNode{2, &node3, &node2}
+	node8 := TreeNode{1, &node7, &node4}
 
 	fmt.Println(levelOrderBottom(&node8))
 
@@ -28,12 +28,8 @@ func levelOrderBottom(root *TreeNode) [][]int {
 	if root == nil {
 		return [][]int{}
 	}
-
 	var res Arr
-	depth := 0
-
-	levelOrderBottom1(root, &res, depth+1)
-
+	levelOrderBottom1(root, &res, 0)
 	return res.List
 }
 
@@ -45,16 +41,12 @@ func levelOrderBottom1(root *TreeNode, res *Arr, d int) {
 	if root == nil {
 		return
 	}
-
 	if (len(res.List) - 1) < d {
-		res.List = append(res.List, []int{root.Val})
+		res.List = append([][]int{{root.Val}}, res.List[:]...)
 	} else {
-		res.List[d] = append(res.List[d], root.Val)
+		tmp := len(res.List) - 1 - d
+		res.List[tmp] = append(res.List[tmp], root.Val)
 	}
-
 	levelOrderBottom1(root.Left, res, d+1)
 	levelOrderBottom1(root.Right, res, d+1)
-
-	fmt.Println(d, len(res.List))
-
 }
