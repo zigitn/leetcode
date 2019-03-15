9350e8a5b9c5a919287c9c26270bbeeb92f5c353
z <gwws@5a6c.me>
2019-03-15 10:55:57 +0800

    0112 path sum

---
+++ easy/0112-path-sum.go
@@ -0,0 +1,21 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	sum -= root.Val
+	if root.Left == nil && root.Right == nil && sum == 0 {
+		return true
+	}
+	return hasPathSum(root.Left, sum) || hasPathSum(root.Right, sum)
+}
